ba73f62e143a05ce0c35f4be1e3271bc21a36302
Xander Juneau <xanderjuneau@gmail.com>
2022-08-31 12:43:27 -0400

    added code from checkpoint

--- src/app.js
+++ src/app.js
@@ -1,17 +1,39 @@
+const morgan = require("morgan")
 const express = require("express");
 const app = express();
-
+const pastes = require("./data/pastes-data")
 // TODO: Follow instructions in the checkpoint to implement ths API.
 
-// Not found handler
+//** App-level **\\
+app.use(morgan("dev"))
+//**            **\\
+
+//** Routes **\\
+app.use("/pastes/:pasteId", (req, res, next) => {
+  const { pasteId } = req.params;
+  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
+  if (foundPaste) {
+    res.json({ data: foundPaste });
+  } else {
+    next(`Paste id not found: ${pasteId}`);
+  }
+});
+
+app.use("/pastes", (req,res,next)=> {
+  res.json({data: pastes})
+})
+//**         **\\
+
+//** Not found handler **\\
 app.use((request, response, next) => {
   next(`Not found: ${request.originalUrl}`);
 });
+//**                    **\\
 
-// Error handler
+//** Error handler **\\
 app.use((error, request, response, next) => {
   console.error(error);
   response.send(error);
 });
-
+//**                 **\\
 module.exports = app;
